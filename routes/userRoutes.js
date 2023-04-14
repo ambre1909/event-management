f5f7ebc1303f40b212c0b45df1664b29ad416af2
akanksha ambre <aga19092001@gmail.com>
2023-04-14 11:15:36 +0530

    url added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,11 @@
 const { registerUser, destroyUsers, addEvent, getEventData } = require("../controllers/userController")
-// const { Protected } = require("../middlewares/auth")
+const { Protected } = require("../middlewares/auth")
 
 const router = require("express").Router()
 router
     .post("/register", registerUser)
-    .post("/add-event", addEvent)
-    .get("/get-event/:date", getEventData)
+    .post("/add-event", Protected, addEvent)
+    .get("/get-event/:date", Protected, getEventData)
     .delete("/destroy", destroyUsers)
 
 
